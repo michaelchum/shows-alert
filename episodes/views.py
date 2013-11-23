12e883653199897cac9d07c519575bc3bb411b53
michaelchum <michaelhochum@gmail.com>
2013-11-23 15:48:57 -0500

    Added view

--- episodes/views.py
+++ episodes/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def index(request):
+	# Request the context of the request
+	# The context contains information such as the client's machine details for example.
+	context = RequestContext(request)

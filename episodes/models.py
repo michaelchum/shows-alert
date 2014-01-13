e92f1da9906a073f3396274c50f76ca34a5e5a9f
Usman EHTESHAM <uehtes@lab6-16.CS.McGill.CA>
2014-01-13 14:49:20 -0500

    Updated Models

--- episodes/models.py
+++ episodes/models.py
@@ -3,17 +3,28 @@ from django.contrib.auth.models import User
 
 class TvShows(models.Model):
 	show_name = models.CharField(max_length=100)
-	show_link = models.URLField(max_length=300)
-	season_episode = models.CharField(max_length=300)
+	url = models.URLField(max_length=300)
+	# season_episode = models.CharField(max_length=300)
 	#name_list = ['uehtesham90@gmail.com', 'usman.ehtesham@mail.mcgill.ca']
 	#name_list = models.ManyToManyField(User)
 	users = models.ManyToManyField(User, blank=True)
 	
 	def __unicode__(self):
-		return '%s %s %s' % (self.show_name, self.season_episode, self.show_link)
+		return '%s ' % (self.show_name)
 
 	def getShowName(self):
 		return self.show_name
+
+	def getUsers(self):
+		return self.users
+
+class Episode(models.Model):
+	episode = models.ForeignKey('TvShows')
+	show_link = models.URLField(max_length=300)
+	season_episode = models.CharField(max_length=300)
+
+	def __unicode__(self):
+		return '%s %s' % (self.season_episode, self.show_link)
 
 	def getShowLink(self):
 		return self.show_link
@@ -21,8 +32,6 @@ class TvShows(models.Model):
 	def getSeasonAndEpisode(self):
 		return self.season_episode
 
-	def getUsers(self):
-		return self.users
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
--- showsgoCrawler.py
+++ showsgoCrawler.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import re
 import urllib2
 from rango.models import TvShows
+from rango.models import Episode
 from sendEmail import send_email
 
 """ 
@@ -46,13 +47,29 @@ def getLatestEpisodes(listOfShows):
 						#listOfShows[show] = url.a.get('href')
 						title = url.a.get('title')
 						stream = url.a.get('href')
+						image = url.a.find('img')['src']
+						print image
 						listOfShows[show].append(stream)						
-						if (TvShows.objects.filter(show_link=stream).count() < 1):
-							p = TvShows(show_name=show , show_link=stream, season_episode=title)
+						p = None
+						if (TvShows.objects.filter(show_name=show).count() < 1):
+						#p = TvShows(show_name=show , show_link=stream, season_episode=title)
+							p = TvShows(show_name=show, picture_link=image)
 							p.save()												
+						#	a = Episode(episode=p, show_link=stream, season_episode=title)
+						#	a.save()
+						#print p
+						else:
+							#p = TvShows.objects.filter(show_name=show)
+							#print type(TvShows.objects.filter(show_name=show))
+							p = TvShows.objects.get(show_name=show)
+						if (Episode.objects.filter(show_link=stream).count() < 1):
+							a = Episode(episode=p, show_link=stream, season_episode=title)
+							a.save()
+
+						
 	return listOfShows
 
-testDict = {'Conan': [], 'Jeopardy': [], 'Regular Show' : [], 'The Voice': []} 
+testDict = {'The Bachelor': [], 'House of Lies': [], 'Jeopardy': [], 'Regular Show' : [], 'The Voice': []} 
 #testDict = {'How i met your mother' : []} 
 
 #to = ['uehtesham90@gmail.com', 'usman.ehtesham@mail.mcgill.ca', 'michaelhochum@gmail.com']
@@ -61,13 +78,18 @@ testDict = getLatestEpisodes(testDict)
 
 print len(TvShows.objects.all())
 
-for i in TvShows.objects.all():
+print len(Episode.objects.all())
+
+for i in Episode.objects.all():
 	# print i.getShowName()
 	# print i.getShowLink()
 	# print i.getSeasonAndEpisode()
-	to = i.getUsers()
-	s = 'Here is link to the latest episode of ' + i.getShowName() +' : \n' + i.getShowLink()
-	send_email(to, i.getSeasonAndEpisode() , s)
+	if i.sent == False:
+		to = i.episode.getUsers()
+		s = 'Here is link to the latest episode of ' + i.getSeasonAndEpisode() +' : \n' + i.getShowLink()
+		send_email(to, i.getSeasonAndEpisode() , s)
+		i.sent = True
+		i.save()
 
 # for key in testDict:
 # 	s = 'Here is link to the latest episodes: \n'

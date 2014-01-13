0282862355bb23cf811ef8bafc72332c1aebf546
michaelchum <michaelhochum@gmail.com>
2014-01-13 14:59:19 -0500

    Mega commit

--- episodes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-#from episodes.models import TvShows, UserProfile
-from episodes.models import TvShows, UserProfile
-admin.site.register(TvShows)
-admin.site.register(UserProfile)
-#admin.site.register(User)
--- rango/models.py
+++ rango/models.py
@@ -3,20 +3,31 @@ from django.contrib.auth.models import User
 
 class TvShows(models.Model):
 	show_name = models.CharField(max_length=100)
-	url = models.URLField(max_length=300)
-	show_link = models.URLField(max_length=300)
-	picture_link = models.URLField(max_length=300)
-	season_episode = models.CharField(max_length=300)
-	likes = models.IntegerField(default=0)
-	users = ['uehtesham90@gmail.com', 'usman.ehtesham@mail.mcgill.ca']
+	url = models.URLField(max_length=300, blank=True)
+	picture_link = models.URLField(max_length=300, blank=True)
+	likes = models.IntegerField(default=5)
+	# season_episode = models.CharField(max_length=300)
+	users = ['uehtesham90@gmail.com']
 	#name_list = models.ManyToManyField(User)
 	#users = models.ManyToManyField(User, blank=True)
 	
 	def __unicode__(self):
-		return '%s %s %s' % (self.show_name, self.season_episode, self.show_link)
+		return '%s %s' % (self.show_name, self.picture_link)
 
 	def getShowName(self):
 		return self.show_name
+
+	def getUsers(self):
+		return self.users
+
+class Episode(models.Model):
+	episode = models.ForeignKey('TvShows')
+	show_link = models.URLField(max_length=300)
+	season_episode = models.CharField(max_length=300)
+	sent = models.BooleanField(default=False)
+
+	def __unicode__(self):
+		return '%s %s %s' % (self.season_episode, self.show_link, self.sent)
 
 	def getShowLink(self):
 		return self.show_link
@@ -24,12 +35,14 @@ class TvShows(models.Model):
 	def getSeasonAndEpisode(self):
 		return self.season_episode
 
-	def getUsers(self):
-		return self.users
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	show_list = models.ManyToManyField('TvShows')
+	#latest_link = models.URLField(max_length=300)
+
+	#def getLatestLink(self):
+	#	return self.latest_link
 
 	def getShowList(self):
 		return self.show_list

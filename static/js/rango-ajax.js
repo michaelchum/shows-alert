d0870bbb9c8038792e34dbdb6b33b38539fc354c
michaelchum <michaelhochum@gmail.com>
2014-01-14 19:01:11 -0500

    Fixed search/JQuery

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -1,28 +1,20 @@
 $(document).ready(function() {
 
-	// JQuery Code
+        $('.rango-add').click(function(){
+                var showid;
+                showid = $(this).attr("data-showid");
+                 $.get('/rango/add_show/', {show_id: showid}, function(data){
+                           $('#list').html(data);
+                       });
+            });
 
-	$('.rango-add').click(function(){
-		var showid;
-		showid = $(this).attr("data-showid");
-		$.get('/rango/add_show/', {show_id: showid}, function(data){
-			$('#list').html(data);
-			//$('#add').hide();
-		});
-	});
 
-<<<<<<< HEAD
-});
-
-// <button id ="add" data-showid="{{ show.id }}" class="btn btn-primary btn-small"><i class="icon-white icon-thumbs-up"></i> Follow</button>
-=======
-	$('#suggestion').keyup(function(){
-        var query;
-        query = $(this).val();
-        $.get('/rango/suggest_category/', {suggestion: query}, function(data){
-         $('#cats').html(data);
+            $('#suggestion').keyup(function(){
+                var query;
+                query = $(this).val();
+                $.get('/rango/suggest_category/', {suggestion: query}, function(data){
+                	$('#cats').html(data);
+                });
         });
-});
 
 });
->>>>>>> FETCH_HEAD
